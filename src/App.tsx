5c08929019e20f259b2aa1cf702eb63fb31a2f42
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-08-20 01:21:37 +0300

    added component

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,16 @@ function App() {
   return (
     <div className="App">
      Hello, samurai! Let's go!
+        <Header/>
     </div>
   );
 }
 
+function Header() {
+    return (
+        <div className="Header">
+            Hello, samurai!
+        </div>
+    );
+}
 export default App;

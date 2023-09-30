32383d6f37a04a7096c120bb34d721e3afed0238
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-09-30 21:15:57 +0300

    added components

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import './App.css';
 import {Header} from "./components/Header";
-import {Technologies} from "./components/Technologies";
-import {Footer} from "./components/Footer";
+import {Nav} from "./components/Nav";
+import {Content} from "./components/Content";
 
 function App() {
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
-        <Header/>
-        <Technologies/>
-        <Footer/>
+    <div className="app-wrapper">
+
+            <Header/>
+
+
+            <Nav/>
+
+
+            <Content/>
+
+
+
 
     </div>
   );
 }
 
 export default App;
+

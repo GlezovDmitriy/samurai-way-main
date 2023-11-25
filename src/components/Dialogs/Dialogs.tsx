caa489976ea204b355a21ddb3ece98a3090acf30
GlezovDmitriy <dmitriy_glezov@mail.ru>
2023-11-25 22:29:34 +0300

    added new NavLink

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,15 +1,25 @@
 import s from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 
 export const Dialogs = () => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <div className={s.dialog}> Dima</div>
-                <div className={s.dialog}> Veronika</div>
-                <div className={s.dialog}> Viktoria</div>
-                <div className={s.dialog}> Ivan</div>
-                <div className={s.dialog}> Alex</div>
-                <div className={s.dialog}> Roman</div>
+                <div className={s.dialog + ' '+ s.active}>
+                    <NavLink to="/dialogs/1">
+                        Dima
+                    </NavLink>
+                    </div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/2">Veronika</NavLink></div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/3">Viktoria</NavLink></div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/4">Ivan</NavLink></div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/5">Alex</NavLink></div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/6">Roman</NavLink></div>
             </div>
             <div className={s.messages}>
                 <div className={s.message}>Hi!</div>
